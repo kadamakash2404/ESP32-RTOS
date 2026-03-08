505de9852d352678873e67d240c3c7741fe0375e
kadamakash2404 <kadamakash2404@gmail.com>
2026-03-08 14:37:33 +0530

    new project added

---
+++ Software_Timer/main/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "driver/gptimer.h"
+#include "esp_log.h"
+
+
+
+void app_main()
+{
+
+}
